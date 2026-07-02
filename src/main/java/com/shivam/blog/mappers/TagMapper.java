a098bacf709a41ed792613d5f4cc4ef1de6f71e2
shivam-sky-57 <shivamkumar7965@gmail.com>
2026-07-02 12:46:30 +0100

    Tag Endpoints added

---
+++ src/main/java/com/shivam/blog/mappers/TagMapper.java
@@ -0,0 +1,28 @@
+package com.shivam.blog.mappers;
+
+import com.shivam.blog.domain.entities.PostStatus;
+import com.shivam.blog.domain.dtos.TagDto;
+import com.shivam.blog.domain.entities.Post;
+import com.shivam.blog.domain.entities.Tag;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+import org.mapstruct.ReportingPolicy;
+
+import java.util.Set;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface TagMapper {
+    @Mapping(target = "postCount", source = "posts", qualifiedByName = "calculatePostCount")
+    TagDto toTagResponse(Tag tag);
+
+    @Named("calculatePostCount")
+    default Integer calculatePostCount(Set<Post> posts) {
+        if(posts == null) {
+            return 0;
+        }
+        return (int) posts.stream()
+                .filter(post -> PostStatus.PUBLISHED.equals(post.getStatus()))
+                .count();
+    }
+}
--- src/main/java/com/shivam/blog/repositories/TagRepository.java
+++ src/main/java/com/shivam/blog/repositories/TagRepository.java
@@ -2,11 +2,16 @@ package com.shivam.blog.repositories;
 
 import com.shivam.blog.domain.entities.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 
 @Repository
 public interface TagRepository extends JpaRepository<Tag, UUID> {
-
+    @Query("SELECT t FROM Tag t LEFT JOIN FETCH t.posts")
+    List<Tag> findAllWithPostCount();
+    List<Tag> findByNameIn(Set<String> names);
 }
